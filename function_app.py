71c5cfbcb37938d8de0d0260c11c9bd3be68f6c8
Zoraxis <b.v.o0077@gmail.com>
2024-01-25 12:55:28 +0100

    azure function test

---
+++ function_app.py
@@ -0,0 +1,16 @@
+from azure import function_app as app, func
+
+from services.routing_service import handle_work_create
+
+@app.function_name(name="HttpTrigger1")
+@app.route(route="/lang-detect")
+def handle_create(req: func.HttpRequest):
+    data = req.params.get('body')
+    # response = handle_work_create(data)
+    return data, 200
+
+# @app.route("/lang-detect/update", methods=["POST"])
+# def handle_update():
+#     data = request.get_json()
+#     response = handle_work_create(data)
+#     return response, 200
