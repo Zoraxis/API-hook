7e8868a61b39f8bdd8285558366068f15f1f2ee0
Zoraxis <b.v.o0077@gmail.com>
2024-01-25 13:34:14 +0100

    azure functions support

--- function_app.py
+++ function_app.py
@@ -1,18 +1,21 @@
-from azure.functions import func
+import azure.functions as func
+from flask import Flask, request
 
 from services.routing_service import handle_work_create
 
-app = func.FunctionApp()
+app = Flask(__name__)
 
-@app.function_name(name="HttpTrigger1")
-@app.route(route="lang-detect")
-def handle_create(req: func.HttpRequest):
-    data = req.params.get('body')
-    # response = handle_work_create(data)
-    return data, 200
+@app.route("/lang-detect", methods=["POST"])
+def handle_create():
+    data = request.get_json()
+    response = handle_work_create(data)
+    return response, 200
 
-# @app.route("/lang-detect/update", methods=["POST"])
-# def handle_update():
-#     data = request.get_json()
-#     response = handle_work_create(data)
-#     return response, 200
+@app.route("/lang-detect/update", methods=["POST"])
+def handle_update():
+    data = request.get_json()
+    response = handle_work_create(data)
+    return response, 200
+
+app = func.WsgiFunctionApp(app=app.wsgi_app, 
+                           http_auth_level=func.AuthLevel.ANONYMOUS)
