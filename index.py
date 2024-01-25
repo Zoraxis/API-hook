50750a651bd7e9873e754db2f2121cc2fd9d467f
Zoraxis <b.v.o0077@gmail.com>
2024-01-25 11:54:41 +0100

    app release

--- index.py
+++ index.py
@@ -4,6 +4,14 @@ from services.routing_service import handle_work_create
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def index():
+    return "ok", 200
+
+@app.route("/lang-detect", methods=["GET"])
+def create_index():
+    return "wrong method", 200
+
 @app.route("/lang-detect", methods=["POST"])
 def handle_create():
     data = request.get_json()
@@ -15,7 +23,6 @@ def handle_update():
     data = request.get_json()
     response = handle_work_create(data)
     return response, 200
-
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
