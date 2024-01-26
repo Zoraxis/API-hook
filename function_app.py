6433e4053412e1b6675663ee131ee2df8f21cae6
Zoraxis <b.v.o0077@gmail.com>
2024-01-26 11:53:06 +0100

    update dependencies

--- function_app.py
+++ function_app.py
@@ -1,43 +1,17 @@
-# import azure.functions as func
-# import logging
-
-# app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
-# @app.route(route="http_trigger")
-# def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-#     logging.info('Python HTTP trigger function processed a request.')
-#     req_body = req.get_json()
-#     response = handle_work_create(req_body)
-
-#     return func.HttpResponse(
-#              response,
-#              status_code=200
-#         )
-
-from services import routing_service
-
 import logging
 import azure.functions as func
+
+from services import routing_service
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="work_trigget")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    req_body = req.get_json()
+    response = handle_work_create(req_body)
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    return func.HttpResponse(
+             response,
              status_code=200
         )
