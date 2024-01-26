9614874b0296998e8a4729a23bf3160b1ee4adf5
Zoraxis <b.v.o0077@gmail.com>
2024-01-26 09:19:42 +0100

    function startup

--- function_app.py
+++ function_app.py
@@ -1,4 +1,5 @@
 import azure.functions as func
+import logging
 from flask import Flask, request
 
 from services.routing_service import handle_work_create
@@ -17,5 +18,15 @@ def handle_update():
     response = handle_work_create(data)
     return response, 200
 
-app = func.WsgiFunctionApp(app=app.wsgi_app, 
-                           http_auth_level=func.AuthLevel.ANONYMOUS)
+# app = func.WsgiFunctionApp(app=app.wsgi_app, 
+                        #    http_auth_level=func.AuthLevel.ANONYMOUS)
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    uri=req.params['uri']
+    with app.test_client() as c:
+        doAction = {
+            "GET": c.get(uri).data,
+            "POST": c.post(uri).data
+        }
+        resp = doAction.get(req.method).decode()
+        return func.HttpResponse(resp, mimetype='text/html')
