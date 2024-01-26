37f20d64f3fb3929e925fab0dcb44a8596ef0e95
Zoraxis <b.v.o0077@gmail.com>
2024-01-26 09:52:41 +0100

    default function app

--- function_app.py
+++ function_app.py
@@ -5,19 +5,41 @@ from ..FlaskApp.wsgi import application
 
 from services.routing_service import handle_work_create
 
-app = Flask(__name__)
+app = func.FunctionApp()
 
-@app.route("/lang-detect", methods=["POST"])
-def handle_create():
-    data = request.get_json()
-    response = handle_work_create(data)
-    return response, 200
+@app.route(route="http_trigger", auth_level=func.AuthLevel.undefined)
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
 
-@app.route("/lang-detect/update", methods=["POST"])
-def handle_update():
-    data = request.get_json()
-    response = handle_work_create(data)
-    return response, 200
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+# app = Flask(__name__)
+
+# @app.route("/lang-detect", methods=["POST"])
+# def handle_create():
+#     data = request.get_json()
+#     response = handle_work_create(data)
+#     return response, 200
+
+# @app.route("/lang-detect/update", methods=["POST"])
+# def handle_update():
+#     data = request.get_json()
+#     response = handle_work_create(data)
+#     return response, 200
 
 # app = func.WsgiFunctionApp(app=app.wsgi_app, 
                         #    http_auth_level=func.AuthLevel.ANONYMOUS)
@@ -32,5 +54,4 @@ def handle_update():
 #         resp = doAction.get(req.method).decode()
 #         return func.HttpResponse(resp, mimetype='text/html')
 
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return func.WsgiMiddleware(application).handle(req, context)
+# main = func.WsgiMiddleware(application).main
