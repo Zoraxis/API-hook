53beffbe37d9986a91cfaa28d917382d094310f9
Zoraxis <b.v.o0077@gmail.com>
2024-01-26 10:29:58 +0100

    function naming fix

--- function_app.py
+++ function_app.py
@@ -5,7 +5,7 @@ from services.routing_service import handle_work_create
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="lang_detect")
+@app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     req_body = req.get_json()
